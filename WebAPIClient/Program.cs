696e2cf087594e4004d6f9ae0054a03f47c4349c
Carlos Zacarias <emii.zacarias@gmail.com>
2023-01-02 20:51:42 -0300

    Agregar archivos de proyecto.

---
+++ WebAPIClient/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace WebAPIClient
+{
+    public class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            var repositories = await ProcessRepositories();
+
+            foreach (var repo in repositories)
+            {
+                Console.WriteLine(repo.Name);
+                Console.WriteLine(repo.Description);
+                Console.WriteLine(repo.GitHubHomeUrl);
+                Console.WriteLine(repo.Homepage);
+                Console.WriteLine(repo.Watchers);
+                Console.WriteLine(repo.LastPush);
+                Console.WriteLine();
+            }
+        }
+
+        private static async Task<List<Repo>> ProcessRepositories()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var stringTask = client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+            var msg = await stringTask;
+
+            var streamTask = client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+            var repositories = await JsonSerializer.DeserializeAsync<List<Repo>>(await streamTask);
+            return repositories;
+        }
+    }
+}
